4a79fa35ae3d522be9af9c4eebcdaf957ca42522
Evgeny <e.tarlo@g.nsu.ru>
2023-10-30 11:12:17 +0700

    upd

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from math import *
 
 colors_list = ['red', 'blue', 'aquamarine', 'green', 'gold', 'purple']  # list of colors
 
-def get_color_choice():  # function to get a color
-    print('Avaluable color:')
+def get_color_choice():
+    """
+    Function to get a color.
+    """
+    print('Available color:')
     for color_menu in range(len(colors_list)):
         print(colors_list[color_menu])
 
@@ -24,7 +27,10 @@ def get_color_choice():  # function to get a color
             right_color = True
     return user_color
 
-def get_num_hexagons():  # function to get a number of hexagons
+def get_num_hexagons():
+    """
+    Function to get a number of hexagons.
+    """
     right_num_hex = False
     user_num_hex = int(input('Please, choose the number of hexagons: '))
     while right_num_hex is False:
@@ -38,74 +44,81 @@ def get_num_hexagons():  # function to get a number of hexagons
             print('Number of hexagons must be a digit, try again: ', end='')
             user_num_hex = int(input())
     return user_num_hex
-def draw_hexagon(x,y,side_len,border_color, fill_color):  # function to draw hexagon
-
-    goto(x,y)
+def draw_hexagon(x, y, side_length, border_color, fill_color):
+    """
+    Function to draw hexagon with parameters:
+    X and Y - drawing coordinates
+    side_length - length of side
+    border_color - color of border
+    fill_color - color of filling
+    """
+    goto(x, y)
     pd()
     begin_fill()
     color(border_color, fill_color)
     lt(30)
-    fd(side_len)
+    fd(side_length)
     lt(60)
-    fd(side_len)
+    fd(side_length)
     lt(60)
-    fd(side_len)
+    fd(side_length)
     lt(60)
-    fd(side_len)
+    fd(side_length)
     lt(60)
-    fd(side_len)
+    fd(side_length)
     lt(60)
-    fd(side_len)
+    fd(side_length)
     lt(30)
     end_fill()
     pu()
-    goto(0,0)
+    goto(0, 0)
+
 
 fill_color1 = get_color_choice()  # get user input for fill color
 fill_color2 = get_color_choice()  # get user input for fill color
 num_hex = get_num_hexagons()  # get user input the number of hexagons
 
-d_hex_len = 500//num_hex
+d_hex_len = 500 // num_hex
 side_len = d_hex_len / sqrt(3)
 
-
 screen = Screen()
-screen.setup(width=1.0, height=1.0) # set up Turtle graphics screen to cover all window
+screen.setup(width=1.0, height=1.0)  # set up Turtle graphics screen to cover all window
 
-
-speed(0) # set the fastest speed
-
+speed(0)  # set the fastest speed
 
 goto(0, 0)
 pu()
 
-
-for itr in range(num_hex - (num_hex//2)): # create left part part of the pattern
+for itr in range(num_hex - (num_hex // 2)):  # create left part of the pattern
     if itr % 2 == 0:
-        for itr2 in range(num_hex): # creating individual hexagons in each row
+        for itr2 in range(num_hex):  # creating individual hexagons in each row
             if itr2 % 2 == 0:
-                draw_hexagon(itr2 * d_hex_len,itr * side_len * 3,side_len,'black',fill_color1)
+                draw_hexagon(itr2 * d_hex_len, itr * side_len * 3, side_len, 'black', fill_color1)
             else:
-                draw_hexagon(itr2 * d_hex_len,itr * side_len * 3,side_len,'black',fill_color2)
+                draw_hexagon(itr2 * d_hex_len, itr * side_len * 3, side_len, 'black', fill_color2)
     else:
-        for itr2 in range(num_hex): # creating individual hexagons in each row
+        for itr2 in range(num_hex):  # creating individual hexagons in each row
             if itr2 % 2 == 0:
-                draw_hexagon(itr2 * d_hex_len,itr * side_len * 3,side_len,'black',fill_color2)
+                draw_hexagon(itr2 * d_hex_len, itr * side_len * 3, side_len, 'black', fill_color2)
             else:
-                draw_hexagon(itr2 * d_hex_len,itr * side_len * 3,side_len,'black',fill_color1)
+                draw_hexagon(itr2 * d_hex_len, itr * side_len * 3, side_len, 'black', fill_color1)
 
-for itr in range(num_hex//2): # create right part part of the pattern
+for itr in range(num_hex // 2):  # create right part of the pattern
     if itr % 2 == 0:
-        for itr2 in range(num_hex): # creating individual hexagons in each row
+        for itr2 in range(num_hex):  # creating individual hexagons in each row
             if itr2 % 2 == 0:
-                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2),(itr * side_len * 3) + (1.5 * side_len),side_len,'black',fill_color1)
+                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2), (itr * side_len * 3) + (1.5 * side_len), side_len,
+                             'black', fill_color1)
             else:
-                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2),(itr * side_len * 3) + (1.5 * side_len),side_len,'black',fill_color2)
+                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2), (itr * side_len * 3) + (1.5 * side_len), side_len,
+                             'black', fill_color2)
     else:
-        for itr2 in range(num_hex): # creating individual hexagons in each row
+        for itr2 in range(num_hex):  # creating individual hexagons in each row
             if itr2 % 2 == 0:
-                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2),(itr * side_len * 3) + (1.5 * side_len),side_len,'black',fill_color2)
+                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2), (itr * side_len * 3) + (1.5 * side_len), side_len,
+                             'black', fill_color2)
             else:
-                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2),(itr * side_len * 3) + (1.5 * side_len),side_len,'black',fill_color1)
+                draw_hexagon((itr2 * d_hex_len) + (d_hex_len / 2), (itr * side_len * 3) + (1.5 * side_len), side_len,
+                             'black', fill_color1)
 
 done()
